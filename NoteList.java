53621220957d6cbf53de98f9995f2016b0e407cd
HoangBarney <122576053+HoangBarney@users.noreply.github.com>
2023-01-12 16:14:12 -0700

    Add files via upload

---
+++ NoteList.java
@@ -0,0 +1,41 @@
+
+/**
+ * Final Project
+ * Thi Cung
+ */
+import java.util.Scanner;
+import java.util.ArrayList;
+public class NoteList implements iSay{
+
+    ArrayList<AddItem> list = new ArrayList<>();
+    
+     public void addItem()
+       { 
+            System.out.println("Enter the name of the item");
+            Scanner input = new Scanner(System.in);
+            String itemName = input.nextLine();
+
+            System.out.println("Enter the price of this item");
+            double itemPrice = input.nextDouble();
+
+            System.out.println("Enter the quantity of item");
+            int itemQty = input.nextInt();
+            AddItem Item = new AddItem(itemName, itemPrice,itemQty);
+            list.add(Item);
+            System.out.println("This Item Added");
+       }
+
+    public void AllList(){
+             for (int counter = 0; counter < list.size(); counter++) {
+                 System.out.println(list.size() + " " + "items");
+
+                 System.out.println(list.toString());
+                }
+             }
+        
+    public void Say()
+       {
+           System.out.println("Thank you for using this adding list. Have a great day!");
+       }
+    
+ }
---
+++ iSay.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class iSay here.
+ * Final Project
+ * Thi Cung
+ */
+public interface iSay
+{
+    public void Say();
+    
+}
